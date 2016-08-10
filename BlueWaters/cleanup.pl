f84a1ea25204890d5efc2d55bcfa06f38580a3b4
Tim Haines <thaines@astro.wisc.edu>
2016-08-10 00:08:07 -0500

    Add configuration parameters

--- BlueWaters/cleanup.pl
+++ BlueWaters/cleanup.pl
@@ -1,21 +1,33 @@
 use strict;
 use warnings;
-use Cwd qw(cwd);
-use ChaNGa qw(%config $base_dir @theta);
+use File::Copy qw(move);
+use Getopt::Long qw(GetOptions);
+use ChaNGa qw(%config $base_dir @theta @size);
 
-my @types = (@ARGV) ? @ARGV : keys %config;
+my $remove_snapshot = 0;
+GetOptions('remove-snapshot!' => \$remove_snapshot);
+
+if (@ARGV < 1) {
+	die "Usage: $0 suffix [base_dir] [--remove-snapshot]\n";
+}
+
+my $snapshot_suffix = $ARGV[0];
+$base_dir = $ARGV[1] if @ARGV;
 
 for my $type (keys %config) {
-	for my $numparticles (keys %{$config{$type}}) {
-		for my $pes_per_node (@{$config{$type}{$numparticles}{'pes_per_node'}}) {
-			for my $threads (@{$config{$type}{$numparticles}{'threads_per_pe'}}) {
-				for my $theta (@theta) {
-					for my $b (@{$config{$type}{$numparticles}{'bucketsize'}}) {
-						my $prefix = "$type+$numparticles+$pes_per_node+$threads+$theta+$b";
-						unlink "$base_dir/$type/$numparticles/$threads/$theta/$b/$prefix.out";
-					}
-				}
-			}
-		}
+my $threads = $config{$type}{'threads_per_pe'};
+for my $numparticles (@size) {
+for my $t (@theta) {
+for my $b (@{$config{$type}{'bucketsize'}}) {
+	my $prefix  = "$type+$numparticles+$threads+$t+$b";
+	my $dir     = "$base_dir/$type/$numparticles/$threads/$t/$b/";
+
+	move("$dir/acc/$prefix.acc.out.000000.acc2","$dir/acc/$prefix.acc");
+	unlink "$dir/$prefix.out.$snapshot_suffix" if $remove_snapshot;
+
+	unlink "$dir/acc/$prefix.acc.out.000000";
+	unlink "$dir/acc/$prefix.acc.out.000000.dom";
+	unlink "$dir/acc/$prefix.acc.out.000000.key";
+	unlink "$dir/acc/$prefix.acc.out.000000.rung";
 	}
-}
+}}}

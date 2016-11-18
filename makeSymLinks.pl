54149030771c8677e10a109e1216722b2193ab43
Tim Haines <thaines.astro@gmail.com>
2016-11-17 22:47:56 -0600

    Simplify link names

--- makeSymLinks.pl
+++ makeSymLinks.pl
@@ -3,7 +3,5 @@ use warnings;
 use ChaNGa qw(%config $base_dir);
 
 for my $type (keys %config) {
-	$type =~ m/^([A-Za-z]+)\d+/;
-	my $clean_type = $1 // $type;
-	symlink("$base_dir/src/$clean_type/changa/ChaNGa","$base_dir/$type/ChaNGa");
+	symlink("$base_dir/src/changa/ChaNGa","$base_dir/$type/ChaNGa");
 }

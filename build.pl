e6b92ff24e8fb0a7f36e5e141b843bccc89be786
Tim Haines <thaines.astro@gmail.com>
2016-12-06 17:02:02 -0600

    Make number of jobs configurable

--- build.pl
+++ build.pl
@@ -9,45 +9,49 @@ sub execute($) {
 	croak "\n\nError executing \n\t'$cmd'\n\n" if ( ( $? >> 8 ) != 0 || $? == -1 || ( $? & 127 ) != 0 );
 }
 
-my ( $cuda, $charm, $changa, $clean, $smp, $float ) = ( '', 1, 1, 0 , 1, 0);
+my ( $cuda, $charm, $changa, $clean, $smp, $float, $njobs ) = ( '', 1, 1, 0 , 1, 0, 2);
 GetOptions(
 	'with-cuda' => \$cuda,
 	'charm!'    => \$charm,
 	'changa!'   => \$changa,
 	'clean'     => \$clean,
 	'smp!'		=> \$smp,
-	'float'		=> \$float
+	'float'		=> \$float,
+	'njobs=i'	=> \$njobs
 ) or exit;
 $cuda = 'cuda' if $cuda;
 $smp  = ($smp) ? 'smp' : '';
 $float = ($float) ? '--enable-float' : '';
 
-if ($clean) {
+if ($charm) {
+	if ($clean) {
+		execute( "
+			cd charm
+			rm -rf bin include lib lib_so tmp VERSION gni-crayxe*
+		" );
+	}
 	execute( "
 		cd charm
-		rm -rf bin include lib lib_so tmp VERSION gni-crayxe*
-	" );
-	execute( "
-		cd changa
-		rm -f *.a *.o config.status Makefile.dep Makefile cuda.mk ChaNGa charmrun
-	" );
-}
-if ($charm) {
-	execute( "
+		
 		# Fix for RCA module issues (https://charm.cs.illinois.edu/redmine/issues/534)
 		export PE_PKGCONFIG_LIBS=cray-rca:$ENV{PE_PKGCONFIG_LIBS}
-		
-		cd charm
+
 		export CUDA_DIR=$ENV{CRAY_CUDATOOLKIT_DIR}
-		./build ChaNGa gni-crayxe hugepages $cuda $smp --enable-lbuserdata -j2 -optimize
+		./build ChaNGa gni-crayxe hugepages $cuda $smp --enable-lbuserdata -j$njobs -optimize
 	" );
 }
 
 if ($changa) {
+	if ($clean) {
+		execute( "
+			cd changa
+			rm -f *.a *.o config.status Makefile.dep Makefile cuda.mk ChaNGa charmrun
+		" );
+	}
 	my $cuda_conf = ($cuda) ? "--with-cuda=$ENV{CRAY_CUDATOOLKIT_DIR}" : '';
 	execute( "
 		cd changa
 		./configure $cuda_conf $float
-		make -j2
+		make -j$njobs
 	" );
 }

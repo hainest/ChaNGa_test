2c208326abdabe389b50f1f9051c82de9a163aab
Tim Haines <thaines.astro@gmail.com>
2018-10-14 07:00:16 -0500

    Add more complex MPI test

--- MPI/t/test.pl
+++ MPI/t/test.pl
@@ -8,12 +8,23 @@ use MPI::Simple;
 MPI::Simple::Init();
 my $rank = MPI::Simple::Comm_rank();
 if ( $rank == 1 ) {
-	my @data = (1, 2, 3);
-	MPI::Simple::Send(\@data, 0, 123);
+	MPI::Simple::Send({'data'=>[17],'name'=>'moo'}, 0, 123);
 }
 else {
-	my $status = 0;
-	my $msg = MPI::Simple::Recv(1, 123, $status);
-	print "$rank received: '@{$msg}'\nstatus = $status\n";
+	my $sender;
+	my $msg = MPI::Simple::Recv(1, 123, \$sender);
+	if(
+		(ref $msg eq ref {}) &&
+		(ref $msg->{'data'} eq ref []) &&
+		@{$msg->{'data'}}[0] == 17 &&
+		$msg->{'name'} eq 'moo' &&
+		$sender == 1
+	) {
+		print "PASSED\n";
+	} else {
+		print "FAILED\n";
+		use Data::Dumper;
+		print Dumper($msg), "\nsender = $sender\n";
+	}
 }
 MPI::Simple::Finalize();

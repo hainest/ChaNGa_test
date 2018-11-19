680e4ce3b73b936f972eed50de470018b9a58e02
Thomas Quinn <trq@astro.washington.edu>
2018-11-19 22:09:44 +0100

    Avoid creating an extra structures subdirectory in copy_dir().
    When using the default ChaNGa::Util::copy_dir on systems with a 'cp' that
    behaves like 'rsync', the trailing slash is needed.
    
    Fixes #10

--- build.pl
+++ build.pl
@@ -124,7 +124,7 @@ sub build_changa {
 	make_path($dest);
 	
 	use ChaNGa::Util qw(copy_dir);
-	copy_dir("$args{'changa-dir'}/../utility/structures", "$dest/structures");
+	copy_dir("$args{'changa-dir'}/../utility/structures/.", "$dest/structures");
 	execute("cd $dest/structures && make clean 2>&1 >/dev/null");
 
 	my $begin = Benchmark->new();
